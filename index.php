9cd64b8f144481fd7926608e9ca83b13c411b87a
Lewis Scott <lscott19@mail.greenriver.edu>
2020-05-25 19:48:49 -0400

    edited submitRecipe form to make sticky

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 //Require the autoload file
 require_once 'vendor/autoload.php';
 //require_once 'model/data-layer.php';
-//require_once 'model/validation-functions.php';
+require_once 'model/validation-functions.php';
 // start session
 session_start();
 
@@ -35,11 +35,64 @@ $f3->route('GET|POST /recipes', function () {
 });
 
 // the submit recipe page
-$f3->route('GET|POST /submitRecipe', function () {
+$f3->route('GET|POST /submitRecipe', function ($f3) {
     //echo '<h1>Initial home page check</h1>';
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        // validate the data
+        $valid = true;
+        // validate name
+        if (!validName($_POST['name'])) {
+            $valid = false;
+            $f3->set('errors["name"]', "Please provide a recipe name");
+        } else {
+            $f3->set('selectedName', $_POST['name']);
+
+        }
+
+        // validate ingredients
+        if (!validIngredients($_POST['ingredients'])) {
+            $valid = false;
+            $f3->set('errors["ingredients"]', "Please provide the ingredients");
+        } else {
+            $f3->set('selectedIngredients', $_POST['ingredients']);
+        }
+
+        // validate directions
+        if (!validDirections($_POST['directions'])) {
+            $valid = false;
+            $f3->set('errors["directions"]', "Please provide the directions");
+        } else {
+            $f3->set('selectedDirections', $_POST['directions']);
+        }
+
+        // validate description
+        if (!validDescription($_POST['description'])) {
+            $valid = false;
+            $f3->set('errors["description"]', "Please provide the description");
+        } else {
+            $f3->set('selectedDescription', $_POST['description']);
+        }
+    }
+
+        /*
+        // data is valid
+        if($valid) {
+            // store the data in the session array
+            $_SESSION['name'] = $_POST['name'];
+            $_SESSION['questions'] = $_POST['questions'];
+
+            // redirect to summary page
+            $f3->reroute('summary');
+            //session_destroy();
+        }
+    }
+         */
     $view = new Template();
     echo $view->render
     ('views/submitRecipe.html');
+
 });
 
 // the admin page
