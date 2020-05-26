87e3e91d64fe34ccb9dcb7889b720293711117b2
Lewis Scott <lscott19@mail.greenriver.edu>
2020-05-25 20:50:20 -0400

    added database connection object

---
+++ model/config-recipe.php
@@ -0,0 +1,5 @@
+<?php
+
+define("DB_DSN", "mysql:dbname=lscottgr_grc");
+define("DB_USERNAME", "lscottgr_grcuser");
+define("DB_PASSWORD", 'Salvor@301');
---
+++ model/database.php
@@ -0,0 +1,86 @@
+<?php
+
+require_once ("config-recipe.php");
+
+class Database
+{
+    //PDO object
+    private $_dbh;
+
+    function __construct()
+    {
+        try {
+            //Create a new PDO connection
+            $this->_dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+            //echo "Connected!";
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    function getRecipes()
+    {
+        //1. Define the query
+        $sql = "SELECT * FROM recipes";
+
+        //2. Prepare the statement
+        $statement = $this->_dbh->prepare($sql);
+
+        //3. Bind the parameters
+
+        //4. Execute the statement
+        $statement->execute();
+
+        //5. Get the result
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+
+    function getDetails($recipeId)
+    {
+        //1. Define the query
+        $sql = "SELECT * 
+                FROM recipes
+                WHERE recipeId = :$recipeId";
+
+        //2. Prepare the statement
+        $statement = $this->_dbh->prepare($sql);
+
+        //3. Bind the parameters
+        $statement->bindParam(':recipeId', $recipeId);
+
+        //4. Execute the statement
+        $statement->execute();
+
+        //5. Get the result
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    function addRecipe($recipe)
+    {
+        //var_dump($student);
+
+        //1. Define the query
+        $sql = "INSERT INTO recipe (recipeName, ingredients, directions, description, image)
+                VALUES (:recipeName, :ingredients, :directions,
+                        :description, :image)";
+
+        //2. Prepare the statement
+        $statement = $this->_dbh->prepare($sql);
+
+        //3. Bind the parameters
+        $statement->bindParam(':recipeName', $recipe->getName());
+        $statement->bindParam(':ingredients', $recipe->getIngredients());
+        $statement->bindParam(':directions', $recipe->getDirections());
+        $statement->bindParam(':description', $recipe->getDescription());
+        $statement->bindParam(':image', $recipe->getImage());
+
+        //4. Execute the statement
+        $statement->execute();
+
+        //Get the key of the last inserted row
+        $id = $this->_dbh->lastInsertId();
+    }
+}
