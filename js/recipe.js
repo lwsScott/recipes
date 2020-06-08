cfdcc1f69a061c4999868ac9deee3ce0eac67725
Lewis Scott <lscott19@mail.greenriver.edu>
2020-06-08 13:31:59 -0400

    added js to build ingredients and direction array

--- js/recipe.js
+++ js/recipe.js
@@ -1,137 +1,117 @@
-/*12-2-2018
-Final Project
-This page uses JQuery to
-create an array of ingredients and display them in a list
-create an array of directions and display them in a list
-Puts the ingredients, directions and recipe name into
-a file to POST to .php on server which stores in a DB for
-later retrieval
-Then have a retrieval function to query the database
-based on search name.
+  /*
+ * Lewis Scott
+ * 5/20/20
+ * filename https://lscott.greenriverdev.com/328/recipes/recipe.js
+ *
+ */
 
-url:  http://lscott.greenriverdev.com/207/final-project/js/recipe.js
+  // create an array of ingredients and display them in a list
+  let ingredients = [];
+  let JSONingredients;
+
+  $("#btnIng").on('click', function() {
+	  var add = $("#ingredient").val();
+
+	  if(ingredients.indexOf(add) == -1 )
+	  {
+		  ingredients.push(add);
+		  ingredients.sort();
+	  }
+
+	  $('#ingList').html("");
+	  for (var i=0; i<ingredients.length; i++)
+	  {
+		  $("#ingList").append("<li>" + ingredients[i] + "</li>" + "<br>");
+	  }
+	  $("#ingredient").val("");
+
+	  // convert that array into a JSON string - puts quotes around the array
+	  JSONingredients = JSON.stringify(ingredients);
+	  document.getElementById('ingredients').value = JSONingredients;
+	  //console.log(JSONingredients);
+  });
+
+  // create an array of directions and display them in a list
+  let directions = [];
+  let JSONdirections;
+  $("#btnDir").on('click', function() {
+	  var add = $("#direction").val();
+	  if(directions.indexOf(add) == -1 )
+	  {
+		  directions.push(add);
+		  directions.sort();
+	  }
+
+	  $('#dirList').html("");
+	  for (var i=0; i<directions.length; i++)
+	  {
+		  $("#dirList").append("<li>" + directions[i] + "</li>" + "<br>");
+	  }
+	  $("#direction").val("");
+
+	  // convert that array into a JSON string - puts quotes around the array
+	  JSONdirections = JSON.stringify(directions);
+	  document.getElementById('directions').value = JSONdirections;
+
+	  //console.log(JSONingredients);
+  });
+
+  // we now have two strings JSONingredients = "[''ing1, "ing2"]"
+  // and JSONdirections "[''dir1, "dir2"]"
+
+  // combine into one array [JSONingredients, JSONdirections, name]
+  // name will come from the input id=recipeName
+  // then stringify that array "[JSONingredients, JSONdirections, name]"
+  // then we can post that to PHP which will store it in a DB table
+
+  // name="recipeName"
+  // name="ingredients"
+  // name="directions"
+  // name="description"
+
+  // this is what a post array looks like
+  // array(7) { ["firstName"]=> string(1) "l" ["lastName"]=> string(1) "s"
+  // ["email"]=> string(11) "me@mail.com" ["phone"]=> string(10) "1234567891"
+  // ["username"]=> string(3) "lew" ["password"]=> string(4) "pswd"
+  // ["confirm"]=> string(4) "pswd" }
+ // $products = array('eggs' => 2.19, 'bacon' => 4.35, 'bagels' => 1.89);
+/*
+  // post the data to the .php file
+  $("#btnSubmit").on('click', function() {
+	  // add the name to the array
+	  let myArr = [JSONingredients, JSONdirections, $('#recipeName').val(), $('#description').val()];
+	  // the post array is an associative array
+	  // create our own associative array
+
+	  let postString = JSON.stringify(myArr);
+	  $.ajax({
+		  url: 'submitRecipe.html',
+		  type: "POST",
+		  dataType: 'text',
+		  data: {recipe: postString}/*,
+		  success: function(result) {
+		  	return result;
+			  //catcher=result;
+			  //console.log(result);
+			  //console.log(catcher);
+			  //console.log($('#searchName').val());
+			  //var object = JSON.parse(result);
+			  //console.log(object);
+			  //$('#output').html("<p>" + object.name + ", " + object.email +
+			  //					", " + object.phone + "</p>");
+		  },
+		  error: function(xhr, resp, text) {
+			  console.log(xhr, resp, text);
+		  }
+	  });
+
+
+	  //$.post('submitRecipe', {recipe: postString});
+	  //var xhr = new XMLHttpRequest();
+	  //xhr.open("POST", 'submitRecipe');
+	  //xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	  //xhr.send((({recipe: postString})
+	  //));
+  // listen for form submit
+//document.getElementById("ingForm").addEventListener("click", addIngredient, false);
 */
-
-	
-	// create an array of ingredients and display them in a list
-	var ingredients = [];
-	var JSONingredients;
-	$("#btnIng").on('click', function() {
-	    var add = $("#ingredient").val();
-	    if(ingredients.indexOf(add) == -1 )
-	    {
-		   ingredients.push(add);
-		   ingredients.sort();
-	    }
-
-	    $('#ingList').html("");
-	    for (var i=0; i<ingredients.length; i++)
-	    {
-		   $("#ingList").append("<li>" + ingredients[i] + "</li>" + "<br>");
-	    }
-	    $("#ingredient").val("");
-	    
-	    	// convert that array into a JSON string - puts quotes around the array
-	    	JSONingredients = JSON.stringify(ingredients);
-		//console.log(JSONingredients);
-        });
-	
-	// create an array of directions and display them in a list
-	var directions = [];
-	var JSONdirections;
-	$("#btnDir").on('click', function() {
-	    var add = $("#direction").val();
-	    if(directions.indexOf(add) == -1 )
-	    {
-		   directions.push(add);
-		   directions.sort();
-	    }
-
-	    $('#dirList').html("");
-	    for (var i=0; i<directions.length; i++)
-	    {
-		   $("#dirList").append("<li>" + directions[i] + "</li>" + "<br>");
-	    }
-	    $("#direction").val("");
-	    
-	    // convert that array into a JSON string - puts quotes around the array
-	    	JSONdirections = JSON.stringify(directions);
-		//console.log(JSONingredients);
-        });
-	
-	
-	// we now have two strings JSONingredients = "[''ing1, "ing2"]"
-	// and JSONdirections "[''dir1, "dir2"]"
-	// combine into one array [JSONingredients, JSONdirections, name]
-	// name will come from the input id=recipeName
-	// then stringify that array "[JSONingredients, JSONdirections, name]"
-	// then we can post that to PHP which will store it in a DB table
-	
-
-
-	// post the data to the .php file	
-	$("#btnSubmit").on('click', function() {
-		// add the name to the array
-		var myArr = [JSONingredients, JSONdirections, $('#recipeName').val()];
-		var postString = JSON.stringify(myArr);
-		$.ajax({
-			url: 'js/recipe.php',
-			type: "POST",
-			dataType: 'text',
-			data: {recipe: postString},
-			success: function(result) {
-				catcher=result;
-				console.log(result);
-				console.log(catcher);
-				//console.log($('#searchName').val());
-				//var object = JSON.parse(result);
-				//console.log(object);
-				//$('#output').html("<p>" + object.name + ", " + object.email +
-				//					", " + object.phone + "</p>");
-			},
-			error: function(xhr, resp, text) {
-				console.log(xhr, resp, text);
-			}
-		});
-	});
-	
-	// now search the database for a recipe name
-	// and return the ingredients, directions, name
-	// parse and display the data.
-	var catcher = [];
-	var object;
-	$("#btnSearch").on('click', function() {
-		//$('#output').html("<p>button is working</p>");
-		//console.log($('#recipeNameSearch').val());
-		$.ajax({
-			url: 'js/recipe-search.php',
-			type: "POST",
-			dataType: 'text',
-			data: {searchName: $('#recipeNameSearch').val()},
-			success: function(result) {
-				catcher=result;
-				console.log(result);
-				console.log(catcher);
-				//console.log($('#searchName').val());
-				object = JSON.parse(result);
-				console.log(object);
-				$('#outputIng').html("");
-				$('#outputDir').html("");
-				$('#output').html("<p>" + object.name);
-				for (var i=0; i<JSON.parse(object.ingredients).length; i++)
-				{
-					$('#outputIng').append("<li>" + JSON.parse(object.ingredients)[i] + "</li>");
-				}
-				for (i=0; i<JSON.parse(object.directions).length; i++)
-				{
-					$('#outputDir').append("<li>" + JSON.parse(object.directions)[i] + "</li>");
-				}
-			},
-			error: function(xhr, resp, text) {
-				console.log(xhr, resp, text);
-			}
-		});
-	});
-	
-	
-	
